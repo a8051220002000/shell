dff623492b8315f5cca5c70695dcdcfc5fd29d86
jimmychen <a8051220002000@gmail.com>
2020-02-10 10:21:57 +0800

    add check_ssl && check domain expirey

---
+++ check_domain_expire_v2.0.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#====con===
+day_controll="30"  #CRT監控剩餘日期小於幾天告警
+#==========
+#2.0 相較第一版，新增無法利用whois查詢到的，1.利用curl去查，如果仍為空輸入/tmp/domain_expired_list
+
+
+rm -rf /tmp/domain_expired_list #其他檔案位於/tmp/domain/ 
+rm -rf /tmp/domain/
+mkdir /tmp/domain/
+
+function find_domain_info(){  
+
+find /usr/local/nginx/ -name '*.conf' -exec grep 'ssl_certificate' {} \; > /tmp/domain/crt_org
+cat /tmp/domain/crt_org|grep -v '#'|grep -v '_key'|awk '{print $2}'|cut -d';' -f1 >>/tmp/domain/crt_storage
+crt_org=$(cat /tmp/domain/crt_org|grep -v '#'|grep -v '_key'|awk '{print $2}'|cut -d';' -f1) 
+  for line in ${crt_org}
+   do
+   cd /usr/local/nginx/conf
+   openssl x509 -in ${line} -enddate </dev/null 2>/dev/null |sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |openssl x509 -text 2>/dev/null|grep DNS|tr "DNS:" "\n"|grep -v 'www.'|tr -s '\n' >> /tmp/domain/domain_temp
+  done
+}
+
+function delete_subdomain(){
+cat /tmp/domain/domain_temp|sort|uniq|sort -n|cut -d',' -f1|grep -v ' ' > /tmp/domain/domain_org #文本處理 去掉重複、空格 逗號
+sub_line=$(cat /tmp/domain/domain_org|wc -l) 
+for (( i=1; i<=${sub_line}; i=i+1 )) #如果是sub domain就變成main domain
+  do
+   #sub_domain去除成為一級域名並且重新排序
+   sub_domain=$(head -n ${i}   /tmp/domain/domain_org|tail -n 1)
+   count=$(echo ${sub_domain} |grep -o '\.'|wc -l)  #變成橫向後計算.出現次數
+  if [[ $count -eq 2 ]];then #確定是子網域
+   main_domain=$(echo ${sub_domain}|cut -d'.' -f2,3) #成為主要網域
+   sed -i ''"${i}"'c '"${main_domain}"''  /tmp/domain/domain_org #取代塞回
+  fi
+  done
+cat /tmp/domain/domain_org|sort|uniq|sort -n > /tmp/domain/domain_storage #此為去掉全部子網域的一級域名清單
+}
+
+function delete_random(){
+random_line=$(cat /tmp/domain/domain_storage|wc -l)
+for (( a=${random_line}; a>=1; a=a-1 ))
+do
+random_domain=$(head -n ${a}  /tmp/domain/domain_storage|tail -n 1) 
+random_count=$(echo ${random_domain} |grep -o '\.'|wc -l)  #變成橫向後計算.出現次數
+if [[ ${random_count} -eq 0 ]];then #沒有 . 等於不是網址
+sed -i ''"${a}d"'' /tmp/domain/domain_storage #遞減移除 避免行數變更
+fi
+done
+}
+
+function domain_expire_check(){
+cat /tmp/domain/domain_storage|while read line
+do
+  domain_time=$(whois ${line}|grep -i Expiry|awk '{print $4}')
+   if [[ -z $domain_time ]];then #這區塊在做whois查不到資料的
+    value=$(curl -sL https://domain.cloudmax.com.tw/whois-search.php|grep csrf|head -1|awk -F "value" '{print  $2}'|cut -d '"' -f 2) 
+#    domain_time=$(curl -sX POST --data "csrf=7fe68cfafddd87438c99cbf81eb1b62a5afe13e3&domain=${line}&Submit="  https://domain.cloudmax.com.tw/whois-search.php|grep -i Expiry|awk '{print $4}'|cut -d'T' -f1) #利用別人家的網站curl查whois資訊
+    domain_time=$(curl -sX POST --data "csrf=${value}&domain=${line}&Submit="  https://domain.cloudmax.com.tw/whois-search.php|grep -i Expiry|awk '{print $4}'|cut -d'T' -f1) #利用別人家的網站curl查whois資訊
+   fi
+  end_date_seconds=$(date '+%s' --date ${domain_time} )
+  now_seconds=`date '+%s'`
+  see="$((($end_date_seconds-$now_seconds)/24/3600))"
+   echo $see >> /tmp/domain/domain_expire
+done
+}
+
+
+function alarm(){ 
+sum_line=$(cat /tmp/domain/domain_storage|wc -l)
+  for (( o=1; o<=${sum_line}; o=o+1 ))
+  do
+    deadline=$(head -n ${o}   /tmp/domain/domain_expire|tail -n 1) #比對剩餘日期
+    info=$(head -n ${o} /tmp/domain/domain_storage|tail -n 1)  #比對域名
+   if [[ ${deadline} -le ${day_controll} ]] ;then #日期小於等於多少的判斷
+    echo $info = $deadline >> /tmp/domain_expired_list
+   fi
+  done
+}
+
+
+find_domain_info;
+delete_subdomain;
+delete_random;
+cat > /tmp/domain_expired_list <<EOF
+此list是為了確認/usr/local/nginx/conf與conf.d 底下CRT檔案去分析出域名，以及其域名是否過期
+會帶上域名及剩下日期，告警觸發時請檢查域名是否過期，或是負值是無法正確whois出域名
+EOF
+domain_expire_check;
+alarm;
+
+cat /tmp/domain_expired_list
---
+++ check_ssl_crt_expire_v2.0.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#====con===
+day_controll="30"  #CRT監控剩餘日期小於幾天告警
+#==========
+#2.0相較第1版，區域變數$crt_org 及 檔案ssl_storage 做出sort 以及uniq的排序，避免一個CRT驗證多次。
+#2.0相較前版本，要做出一支CRT給多個域名的監控
+RED_C='\E[1;31m'
+RESET='\E[0m'
+rm -rf /tmp/ssl_list #其他檔案位於/tmp/SSL/ 
+rm -rf /tmp/SSL/
+mkdir /tmp/SSL/
+
+function find_ssl_info(){  #此function功能:比對現在時間與/usr/local/nginx/conf conf.d 兩個資料夾底下crt到期日 的差
+find /usr/local/nginx/ -name '*.conf' -exec grep 'ssl_certificate' {} \; > /tmp/SSL/ssl_org
+cat /tmp/SSL/ssl_org|grep -v '#'|grep -v '_key'|awk '{print $2}'|cut -d';' -f1|sort|uniq >>/tmp/SSL/ssl_storage
+crt_org=$(cat /tmp/SSL/ssl_org|grep -v '#'|grep -v '_key'|awk '{print $2}'|cut -d';' -f1|sort |uniq) 
+  for line in ${crt_org}
+  do
+   cd /usr/local/nginx/conf
+    end_date=$(openssl x509 -in ${line} -enddate </dev/null 2>/dev/null |           sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |       openssl x509 -text 2>/dev/null |       sed -n 's/ *Not After : *//p')
+    end_date_seconds=$(date '+%s' --date "$end_date")
+    now_seconds=`date '+%s'`
+    #echo "($end_date_seconds-$now_seconds)/24/3600" | bc
+    see="$((($end_date_seconds-$now_seconds)/24/3600))"  #沒bc算法
+   echo $see >> /tmp/SSL/ssl_expire
+  done
+}
+
+function alarm(){ #利用/tmp/SSL/ssl_storage 行數以及比對 /tmp/SSL/ssl_expiret 產生一個檔案
+sum_line=$(cat /tmp/SSL/ssl_storage|wc -l)
+  for (( i=1; i<=${sum_line}; i=i+1 ))
+  do
+    deadline=$(head -n ${i} /tmp/SSL/ssl_expire|tail -n 1) #比對日期 當下迴圈某一行
+    info=$(head -n ${i} /tmp/SSL/ssl_storage|tail -n 1)  #比對CRT名稱 當下迴圈某一行
+    all_domain=$(openssl x509 -in ${info} -noout -text|grep -oP '(?<=DNS:|IP Address:)[^,]+'|sort -uV)
+    if [[ ${deadline} -le ${day_controll} ]] ;then #日期小於多少的判斷
+     echo -e "${info} = 剩餘${deadline}日 ${RED_C} 影響域名  $(echo -e ${all_domain}|sed "s/\n//g") ${RESET}">>/tmp/ssl_list
+   fi
+  done
+}
+
+find_ssl_info;
+cat > /tmp/ssl_list <<EOF
+此list是為了確認/usr/local/nginx/conf與conf.d 底下CRT檔案是否過期
+會帶上檔名及剩下日期，告警觸發時請檢查nginx.conf server_name 對應之CRT都要過期了
+EOF
+alarm;
+
+cat /tmp/ssl_list
+
