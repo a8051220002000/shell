823f429501821d0bfd87a2ddebc6ddcd89095d31
a8051220002000 <53286986+a8051220002000@users.noreply.github.com>
2020-02-10 15:36:49 +0800

    Update check_domain_expire_v2.0.sh
    還要有zabbix設定
    UserParameter=domain_exipre,sudo cat /tmp/domain_expired_list|wc -l
    
    {Template DOMAIN CRT:domain_exipre.max(#1)}>2 or {Template DOMAIN CRT:domain_exipre.nodata(10m)}=1

--- check_domain_expire_v2.0.sh
+++ check_domain_expire_v2.0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#用途，利用/usr/local/nginx/conf/nginx.conf Hostname來分析出域名到期日
 #====con===
 day_controll="30"  #CRT監控剩餘日期小於幾天告警
 #==========
@@ -15,15 +16,15 @@ find /usr/local/nginx/ -name '*.conf' -exec grep 'ssl_certificate' {} \; > /tmp/
 cat /tmp/domain/crt_org|grep -v '#'|grep -v '_key'|awk '{print $2}'|cut -d';' -f1 >>/tmp/domain/crt_storage
 crt_org=$(cat /tmp/domain/crt_org|grep -v '#'|grep -v '_key'|awk '{print $2}'|cut -d';' -f1) 
   for line in ${crt_org}
-   do
-   cd /usr/local/nginx/conf
-   openssl x509 -in ${line} -enddate </dev/null 2>/dev/null |sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |openssl x509 -text 2>/dev/null|grep DNS|tr "DNS:" "\n"|grep -v 'www.'|tr -s '\n' >> /tmp/domain/domain_temp
-  done
+    do
+    cd /usr/local/nginx/conf
+    openssl x509 -in ${line} -enddate </dev/null 2>/dev/null |sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |openssl x509 -text 2>/dev/null|grep DNS|tr "DNS:" "\n"|grep -v 'www.'|tr -s '\n' >> /tmp/domain/domain_temp
+    done
 }
 
 function delete_subdomain(){
-cat /tmp/domain/domain_temp|sort|uniq|sort -n|cut -d',' -f1|grep -v ' ' > /tmp/domain/domain_org #文本處理 去掉重複、空格 逗號
-sub_line=$(cat /tmp/domain/domain_org|wc -l) 
+ cat /tmp/domain/domain_temp|sort|uniq|sort -n|cut -d',' -f1|grep -v ' ' > /tmp/domain/domain_org #文本處理 去掉重複、空格 逗號
+ sub_line=$(cat /tmp/domain/domain_org|wc -l) #計算幾行，用於下面決定要跑幾次for迴圈
 for (( i=1; i<=${sub_line}; i=i+1 )) #如果是sub domain就變成main domain
   do
    #sub_domain去除成為一級域名並且重新排序
@@ -34,35 +35,35 @@ for (( i=1; i<=${sub_line}; i=i+1 )) #如果是sub domain就變成main domain
    sed -i ''"${i}"'c '"${main_domain}"''  /tmp/domain/domain_org #取代塞回
   fi
   done
-cat /tmp/domain/domain_org|sort|uniq|sort -n > /tmp/domain/domain_storage #此為去掉全部子網域的一級域名清單
+ cat /tmp/domain/domain_org|sort|uniq|sort -n > /tmp/domain/domain_storage #此為去掉全部子網域的一級域名清單
 }
 
 function delete_random(){
-random_line=$(cat /tmp/domain/domain_storage|wc -l)
-for (( a=${random_line}; a>=1; a=a-1 ))
-do
-random_domain=$(head -n ${a}  /tmp/domain/domain_storage|tail -n 1) 
-random_count=$(echo ${random_domain} |grep -o '\.'|wc -l)  #變成橫向後計算.出現次數
-if [[ ${random_count} -eq 0 ]];then #沒有 . 等於不是網址
-sed -i ''"${a}d"'' /tmp/domain/domain_storage #遞減移除 避免行數變更
-fi
-done
+ random_line=$(cat /tmp/domain/domain_storage|wc -l)
+  for (( a=${random_line}; a>=1; a=a-1 ))
+   do
+    random_domain=$(head -n ${a}  /tmp/domain/domain_storage|tail -n 1) #從後面算回來，避免第一行被殺掉以後，第二行又變第一行
+    random_count=$(echo ${random_domain} |grep -o '\.'|wc -l)  #變成橫向後計算.出現次數
+   if [[ ${random_count} -eq 0 ]];then #沒有 . 等於不是網址
+    sed -i ''"${a}d"'' /tmp/domain/domain_storage #遞減移除 避免行數變更
+   fi
+  done
 }
 
 function domain_expire_check(){
-cat /tmp/domain/domain_storage|while read line
-do
-  domain_time=$(whois ${line}|grep -i Expiry|awk '{print $4}')
+  cat /tmp/domain/domain_storage|while read line
+   do
+    domain_time=$(whois ${line}|grep -i Expiry|awk '{print $4}')
    if [[ -z $domain_time ]];then #這區塊在做whois查不到資料的
     value=$(curl -sL https://domain.cloudmax.com.tw/whois-search.php|grep csrf|head -1|awk -F "value" '{print  $2}'|cut -d '"' -f 2) 
-#    domain_time=$(curl -sX POST --data "csrf=7fe68cfafddd87438c99cbf81eb1b62a5afe13e3&domain=${line}&Submit="  https://domain.cloudmax.com.tw/whois-search.php|grep -i Expiry|awk '{print $4}'|cut -d'T' -f1) #利用別人家的網站curl查whois資訊
+    #domain_time=$(curl -sX POST --data "csrf=7fe68cfafddd87438c99cbf81eb1b62a5afe13e3&domain=${line}&Submit="  https://domain.cloudmax.com.tw/whois-search.php|grep -i Expiry|awk '{print $4}'|cut -d'T' -f1) #利用別人家的網站curl查whois資訊
     domain_time=$(curl -sX POST --data "csrf=${value}&domain=${line}&Submit="  https://domain.cloudmax.com.tw/whois-search.php|grep -i Expiry|awk '{print $4}'|cut -d'T' -f1) #利用別人家的網站curl查whois資訊
    fi
-  end_date_seconds=$(date '+%s' --date ${domain_time} )
-  now_seconds=`date '+%s'`
-  see="$((($end_date_seconds-$now_seconds)/24/3600))"
-   echo $see >> /tmp/domain/domain_expire
-done
+    end_date_seconds=$(date '+%s' --date ${domain_time} )
+    now_seconds=`date '+%s'`
+    see="$((($end_date_seconds-$now_seconds)/24/3600))"
+    echo $see >> /tmp/domain/domain_expire
+    done
 }
 
 
